d2da1336af4417292f2d5a43c68bbb3d845666d6
m-fe-dev <m@kos.co.th>
2026-06-23 15:04:32 +0700

    ✨ feat: implement page_not_found

--- application/themes/kos/blocks/form/templates/contact_form/view.php
+++ application/themes/kos/blocks/form/templates/contact_form/view.php
@@ -895,12 +895,12 @@ $introId = 'contact-form-intro-' . $bID;
         </div>
 
         <div class="contact-form-template__actions">
-            <button type="submit" name="Submit" value="<?php echo h(t($survey->submitText)); ?>" class="contact-form-template__submit contact__submit">
-                <svg class="contact-form-template__submit-cap contact__submit-cap" width="17" height="38" viewBox="0 0 17 38" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
+            <button type="submit" name="Submit" value="<?php echo h(t($survey->submitText)); ?>" class="contact-form-template__submit contact__submit btn btn--block">
+                <svg class="contact-form-template__submit-cap contact__submit-cap btn__cap btn__cap--left" width="17" height="38" viewBox="0 0 17 38" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
                     <path d="M0 9.65686C0 8.59599 0.421427 7.57857 1.17157 6.82843L6.82843 1.17157C7.57857 0.421426 8.59599 0 9.65685 0L17 0V38H9.65685C8.59599 38 7.57857 37.5786 6.82843 36.8284L1.17157 31.1716C0.421426 30.4214 0 29.404 0 28.3431V9.65686Z" fill="#ECEAE5" />
                 </svg>
-                <span class="contact-form-template__submit-label contact__submit-label"><?php echo h(t($survey->submitText)); ?></span>
-                <svg class="contact-form-template__submit-cap contact__submit-cap" width="17" height="38" viewBox="0 0 17 38" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
+                <span class="contact-form-template__submit-label contact__submit-label btn__label"><?php echo h(t($survey->submitText)); ?></span>
+                <svg class="contact-form-template__submit-cap contact__submit-cap btn__cap btn__cap--right" width="17" height="38" viewBox="0 0 17 38" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
                     <path d="M17 9.65686C17 8.59599 16.5786 7.57857 15.8284 6.82843L10.1716 1.17157C9.42143 0.421426 8.40401 0 7.34315 0L0 0V38H7.34315C8.40401 38 9.42143 37.5786 10.1716 36.8284L15.8284 31.1716C16.5786 30.4214 17 29.404 17 28.3431V9.65686Z" fill="#ECEAE5" />
                 </svg>
             </button>
--- application/themes/kos/page_not_found.php
+++ application/themes/kos/page_not_found.php
@@ -2,10 +2,19 @@
 defined('C5_EXECUTE') or die('Access Denied.');
 $this->inc('elements/header.php');
 ?>
-<main class="policy" id="main-content">
-  <section class="policy__section">
-    <h1 class="policy__section-title"><?= t('404 Error') ?></h1>
-    <p><?= t('Page not found.') ?></p>
-  </section>
+<main class="error-page" id="main-content" aria-labelledby="error-page-title">
+  <div class="error-page__content">
+    <h1 class="error-page__title" id="error-page-title"><?= t('404') ?></h1>
+    <p class="error-page__text"><?= t('This page may have moved, been removed, or is still loading in another dimension.') ?></p>
+    <a class="btn error-page__cta" href="/">
+      <svg class="btn__cap btn__cap--left" width="17" height="38" viewBox="0 0 17 38" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
+        <path d="M0 9.65686C0 8.59599 0.421427 7.57857 1.17157 6.82843L6.82843 1.17157C7.57857 0.421426 8.59599 0 9.65685 0L17 0V38H9.65685C8.59599 38 7.57857 37.5786 6.82843 36.8284L1.17157 31.1716C0.421426 30.4214 0 29.404 0 28.3431V9.65686Z" fill="#ECEAE5" />
+      </svg>
+      <span class="btn__label"><?= t('Back to homepage') ?></span>
+      <svg class="btn__cap btn__cap--right" width="17" height="38" viewBox="0 0 17 38" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
+        <path d="M17 9.65686C17 8.59599 16.5786 7.57857 15.8284 6.82843L10.1716 1.17157C9.42143 0.421426 8.40401 0 7.34315 0L0 0V38H7.34315C8.40401 38 9.42143 37.5786 10.1716 36.8284L15.8284 31.1716C16.5786 30.4214 17 29.404 17 28.3431V9.65686Z" fill="#ECEAE5" />
+      </svg>
+    </a>
+  </div>
 </main>
-<?php $this->inc('elements/footer.php'); ?>
+<?php $this->inc('elements/footer_bottom.php'); ?>
